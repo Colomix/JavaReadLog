c12db71225f8da6ba1dfe26b8b219f998e542b97
o.popkov <zerooleg@yandex.ru>
2016-02-23 18:50:11 +0500

    Добавлены файлы .gitignore, pom.xml, Main.java.

---
+++ src/main/java/ru/main/JavaReadLog/Main.java
@@ -0,0 +1,10 @@
+package ru.main.JavaReadLog;
+
+/**
+ * Created by Олег on 23.02.2016.
+ */
+public class Main {
+    public static void main(String... args) {
+        System.out.print("It is works");
+    }
+}
